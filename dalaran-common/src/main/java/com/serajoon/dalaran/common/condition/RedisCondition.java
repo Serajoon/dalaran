da53e38667fa8856888d040a91d5ef947eb2ee42
serajoon <serajoon@163.com>
2019-01-20 18:32:29 +0800

    rename MyHttpUtils to MyNetUtils

--- dalaran-common/src/main/java/com/serajoon/dalaran/common/condition/RedisCondition.java
+++ dalaran-common/src/main/java/com/serajoon/dalaran/common/condition/RedisCondition.java
@@ -1,7 +1,7 @@
 package com.serajoon.dalaran.common.condition;
 
 
-import com.serajoon.dalaran.common.util.MyHttpUtils;
+import com.serajoon.dalaran.common.util.MyNetUtils;
 import com.serajoon.dalaran.common.util.MyLogUtils;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -16,7 +16,7 @@ public class RedisCondition implements Condition {
     public boolean matches(@NotNull ConditionContext context, @NotNull AnnotatedTypeMetadata metadata) {
         String ip = context.getEnvironment().getProperty("spring.redis.host","localhost");
         String port = context.getEnvironment().getProperty("spring.redis.port","6379");
-        boolean connectable = MyHttpUtils.isHostConnectable(ip, Integer.valueOf(port));
+        boolean connectable = MyNetUtils.isIpAndPortReachable(ip, Integer.valueOf(port));
         if(!connectable){
             log.error("redis is not unable to connect");
         }
--- dalaran-common/src/main/java/com/serajoon/dalaran/common/util/MyNetUtils.java
+++ dalaran-common/src/main/java/com/serajoon/dalaran/common/util/MyNetUtils.java
@@ -1,17 +1,26 @@
 package com.serajoon.dalaran.common.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
-public abstract class MyHttpUtils {
+@Slf4j
+public abstract class MyNetUtils {
 
-    public static boolean isHostConnectable(String host, int port) {
+    /**
+     * 判断一个指定ip和端口的服务是否可用
+     * @param host ip
+     * @param port prot
+     * @return
+     */
+    public static boolean isIpAndPortReachable(String host, int port) {
         Socket socket = new Socket();
         try {
             socket.connect(new InetSocketAddress(host, port),100);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("{}:{} is not connectable",host,port);
             return false;
         } finally {
             try {
