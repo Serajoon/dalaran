8f5197958206cd2626db5896e317c9e1ecc3445d
Serajoon <Serajoon@163.com>
2019-03-26 19:53:55 +0800

    Update MyNetUtils.java

--- dalaran-common/src/main/java/com/serajoon/dalaran/common/util/MyNetUtils.java
+++ dalaran-common/src/main/java/com/serajoon/dalaran/common/util/MyNetUtils.java
@@ -3,6 +3,7 @@ package com.serajoon.dalaran.common.util;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
@@ -11,17 +12,22 @@ public abstract class MyNetUtils {
 
     /**
      * 判断一个指定ip和端口的服务是否可用
+     *
      * @param host ip
      * @param port prot
      * @return
      */
     public static boolean isIpAndPortReachable(String host, int port) {
+        boolean result = false;
+        if (!isPing(host)) {
+            return false;
+        }
         Socket socket = new Socket();
         try {
-            socket.connect(new InetSocketAddress(host, port),100);
+            socket.connect(new InetSocketAddress(host, port), 100);
+            result = socket.isConnected();
         } catch (IOException e) {
-            log.error("{}:{} is not connectable",host,port);
-            return false;
+            log.error("{}:{} is not connectable", host, port);
         } finally {
             try {
                 socket.close();
@@ -29,6 +35,23 @@ public abstract class MyNetUtils {
                 e.printStackTrace();
             }
         }
-        return true;
+        return result;
+    }
+
+    /**
+     * ping一个ip地址
+     *
+     * @param ip ip地址
+     * @return true:ping通 false:ping失败
+     */
+    public static boolean isPing(String ip) {
+        boolean status = false;
+        if (ip != null) {
+            try {
+                status = InetAddress.getByName(ip).isReachable(200);
+            } catch (IOException ignored) {
+            }
+        }
+        return status;
     }
 }
